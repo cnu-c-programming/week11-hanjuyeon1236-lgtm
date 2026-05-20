cf8150b437607d95b4d6c62a509767cad52f213d
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-05-20 20:08:33 +0900

    Update 2-2.c

--- code/2-2.c
+++ code/2-2.c
@@ -6,12 +6,12 @@ enum Status { OK = 0, ERR = -1, BUSY = 1 };
 
 int main(void) {
   printf("Red = %d\n", RED);
-  printf("FREEN = %d\n", GREEN);
+  printf("GREEN = %d\n", GREEN);
   printf("BLUE = %d\n\n", BLUE);
 
   printf("OK = %d\n", OK);
   printf("ERR = %d\n", ERR);
-  printf("BUSYS = %d\n", BUSY);
+  printf("BUSY = %d\n", BUSY);
 
   return 0;
 }
