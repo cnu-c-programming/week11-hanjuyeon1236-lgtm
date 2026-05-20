3e993ba803fd0858df8d2778b29673684db03e64
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-05-20 20:14:54 +0900

    Update 3-2.c

--- code/3-2.c
+++ code/3-2.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
-#include <sgdlib.h>
+#include <stdlib.h>
 
-int cmp_asc( ) {
-
+int cmp_asc(const void *a, const void *b) {
+  int num1 = *(const int *)a;
+    int num2 = *(const int *)b;
+    
+    return num1 - num2;
 }
 
-int cmp_desc( ) {
+int cmp_desc(const void *a, const void *b) {
+  int num1 = *(const int *)a;
+    int num2 = *(const int *)b;
+    
+    return num2 - num1;
 
 }
 
 void print_arr(int *arr, int size) {
-  for (int i = 0; i < sizel i++) {
+  for (int i = 0; i < size; i++) {
     printf("%d ", arr[i]);
   }
   printf("\n");
@@ -20,10 +27,10 @@ int main(void) {
   int arr[] = {5, 2, 8, 1, 9, 3};
   int size = 6;
 
-  qsort( );
+  qsort(arr, size, sizeof(int), cmp_asc);
   print_arr(arr, size);
 
-  qsort( );
+  qsort(arr, size, sizeof(int), cmp_desc);
   print_arr(arr, size);
 
   return 0;
